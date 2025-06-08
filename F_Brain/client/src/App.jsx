46bfb9c3399ba96d6a914798924e63d38515879a
Merveille19 <merveillelontchi@gmail.com>
2025-06-08 23:59:16 +0200

    gestion des mes differentes pages

--- F_Brain/client/src/App.jsx
+++ F_Brain/client/src/App.jsx
@@ -1,27 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import StudentRegister from './pages/Student/Register';
-import StudentProfile from './pages/Student/Profile';
-import CompanyRegister from './pages/Company/Register';
-import CompanyDashboard from './pages/Company/Dashboard';
-import AdminDashboard from './pages/Admin/Dashboard';
-import CompanyList from './pages/CompanyList';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages générales
+import HomePage from "./pages/HomePage";
+import CompanyList from "./pages/CompanyList";
+
+// Étudiant
+import StudentRegister from "./pages/Student/Register";
+import StudentProfile from "./pages/Student/Profile";
+
+// Entreprise
+import CompanyRegister from "./pages/Company/Register";
+import CompanyDashboard from "./pages/Company/Dashboard";
+
+// Admin
+import AdminDashboard from "./pages/Admin/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/student/register" element={<StudentRegister />} />
-        <Route path="/student/profile" element={<StudentProfile />} />
-        <Route path="/company/register" element={<CompanyRegister />} />
-        <Route path="/company/dashboard" element={<CompanyDashboard />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/companies" element={<CompanyList />} />
+        <Route path="/entreprises" element={<CompanyList />} />
+
+        {/* Étudiant */}
+        <Route path="/etudiant/inscription" element={<StudentRegister />} />
+        <Route path="/etudiant/profil" element={<StudentProfile />} />
+
+        {/* Entreprise */}
+        <Route path="/entreprise/inscription" element={<CompanyRegister />} />
+        <Route path="/entreprise/dashboard" element={<CompanyDashboard />} />
+
+        {/* Admin */}
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
 
